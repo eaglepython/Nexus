a279a576f6803ad090f7929e35e1f43a4bd9e63b
eaglepython <bidias_consulting@outlook.com>
2025-09-12 13:06:04 -0500

    Update base path for Nexus repository GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/The-Nexus-Weather-App/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Nexus/' : '/',
   plugins: [react()],
   server: {
     port: 5173,
